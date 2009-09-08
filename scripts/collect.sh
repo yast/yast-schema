ad50bf2fade1887b8cec08762ae2cd67cb384f1d
Martin Vidner <mvidner@suse.cz>
2009-09-08 08:26:40 +0000

    Eval only X-SuSE-YaST-AutoInst*. In particular avoid X-SuSE-YaST-Keywords, it may contain spaces and that makes the script fail.
    svn path=/trunk/schema/; revision=58577

--- scripts/collect.sh
+++ scripts/collect.sh
@@ -31,7 +31,7 @@ for desktop in `find $DESKTOP_DIR $DESKTOP_DIR2 -name '*.desktop' | LC_ALL=C sor
     unset X_SuSE_YaST_AutoInstPath
     unset X_SuSE_YaST_AutoInstOptional
 
-    eval $(grep "^X-SuSE-YaST" $desktop | sed -e 's/X-SuSE-YaST-/X_SuSE_YaST_/' )
+    eval $(grep "^X-SuSE-YaST-AutoInst" $desktop | sed -e 's/X-SuSE-YaST-/X_SuSE_YaST_/' )
 
     if [ -z "$X_SuSE_YaST_AutoInstResource" ]; then
         resource=`basename $desktop .desktop`
